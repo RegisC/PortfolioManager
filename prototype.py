85bba05b8f44567370bcbc2627b75edbf0482a71
Regis <lorrain-74@yahoo.com>
2018-10-23 14:45:40 +0200

    Added position summary

--- prototype.py
+++ prototype.py
@@ -1,4 +1,5 @@
 import config
+import locale
 import logging
 import os
 import pandas as pd
@@ -41,8 +42,25 @@ def get_security_code(name, reference):
     else:
         return _securities[position]['Code'].item()
 
+def get_security_name(s):
+    global _securities
+    if (_securities is None):
+        _securities = pd.read_csv(config.SECURITIES_STATIC_DATA_FILENAME,
+            index_col=None,
+            header=0)
+        logger.debug("Loaded securities DataFrame of size " + str(_securities.shape) + ".")
+    position = _securities.loc[_securities['Code'].str.lower()==s.lower(), 'Description']
+    if (position.empty):
+        return s
+    else:
+        return position.item()
+
 def enrich_transaction_table(df):
+    # Add column with security code
     df['Code'] = df.apply(lambda row: get_security_code(row['Description'], row['Reference']), axis = 1)
+    # Convert quantity to a signed number
+    df['QuantityAsNumber'] = pd.to_numeric(df['Quantity'].str.replace(',',''))
+    df.loc[df['Reference'].str.startswith('S'), 'QuantityAsNumber'] *= -1
 
 def load_transactions(filelist):
     all_dfs = []
@@ -50,15 +68,20 @@ def load_transactions(filelist):
         logger.debug("Loading transactions from file " + f + ".")
         df = pd.read_csv(f, index_col=None, header=0)
         all_dfs.append(df)
-    output_df = pd.concat(all_dfs, sort=True)
+    output_df = pd.concat(all_dfs, ignore_index=True)
     return output_df
+
+def get_positions_as_of(df, as_of_date = None):
+    securities = df['Code'].unique()
+    positions = { get_security_name(s):df.loc[df['Code'] == s,'QuantityAsNumber'].sum() for s in securities }
+    return positions
 
 if __name__ == '__main__':
     df = load_transactions(config.TRANSACTION_FILES)
-    df.to_csv("C:\\temp\\df.txt")
     logger.debug("Aggregate DataFrame has size " + str(df.shape) + ".")
     enrich_transaction_table(df)
+    df.to_csv("C:\\temp\\df.txt")
     logger.debug("Post-processed DataFrame has size " + str(df.shape) + ".")
-    print(df.columns.tolist())
     print("Entries without security code:")
     print(df[df['Code']==''])
+    print(get_positions_as_of(df))
