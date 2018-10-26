78c541f921f8d96a9ebce1bca66c45e3b2d10113
Regis <lorrain-74@yahoo.com>
2018-10-26 11:36:41 +0200

    Finalize basic support for ISA + F&S

--- prototype.py
+++ prototype.py
@@ -13,10 +13,11 @@ _securities = None
 
 def get_security_code(name, reference):
     # Check if we are given a cash entry
-    if ((reference.upper() in ['MANAGE FEE', 'INTEREST', 'REG. SAVER', 'FPD', 'CARD WEB'])
+    if ((reference.upper() in ['MANAGE FEE', 'INTEREST', 'REG. SAVER', 'FPD', 'CARD WEB', 'TRANSFER'])
         or reference.upper().startswith('LSJ')):
         return 'CASH'
     elif ((name.upper() == 'INTERNAL')
+        or name.upper().startswith('LOYALTY')
         or name.upper().startswith('INCOME RE-INVEST')
         or name.upper().startswith('PRODUCT TRANSFER')
         or name.upper().startswith('DIVIDEND TRANSFER')
@@ -59,29 +60,36 @@ def enrich_transaction_table(df):
     # Add column with security code
     df['Code'] = df.apply(lambda row: get_security_code(row['Description'], row['Reference']), axis = 1)
     # Convert quantity to a signed number
-    df['QuantityAsNumber'] = pd.to_numeric(df['Quantity'].str.replace(',',''))
+    df['Quantity'] = df['Quantity'].astype('str').str.replace(',','')
+    df['QuantityAsNumber'] = pd.to_numeric(df['Quantity'], errors='coerce')
     df.loc[df['Reference'].str.startswith('S'), 'QuantityAsNumber'] *= -1
 
 def load_transactions(filelist):
     all_dfs = []
     for f in filelist:
-        logger.debug("Loading transactions from file " + f + ".")
         df = pd.read_csv(f, index_col=None, header=0)
+        logger.debug("Loaded DataFrame of size " + str(df.shape) + " from file " + f + ".")
         all_dfs.append(df)
     output_df = pd.concat(all_dfs, ignore_index=True)
     return output_df
 
 def get_positions_as_of(df, as_of_date = None):
+    # Get unique list of securities codes
     securities = df['Code'].unique()
-    positions = { get_security_name(s):df.loc[df['Code'] == s,'QuantityAsNumber'].sum() for s in securities }
+    # Create dictionary with securities as keys and position as values
+    positions = { get_security_name(s):round(df.loc[df['Code'] == s,'QuantityAsNumber'].sum(),4) for s in securities }
     return positions
 
-if __name__ == '__main__':
+def main():
     df = load_transactions(config.TRANSACTION_FILES)
-    logger.debug("Aggregate DataFrame has size " + str(df.shape) + ".")
     enrich_transaction_table(df)
-    df.to_csv("C:\\temp\\df.txt")
+    try:
+        df.to_csv("C:\\temp\\df.csv")
+    except:
+        logger.error("Could not save data to file.")
     logger.debug("Post-processed DataFrame has size " + str(df.shape) + ".")
-    print("Entries without security code:")
-    print(df[df['Code']==''])
-    print(get_positions_as_of(df))
+    for k, v in get_positions_as_of(df).items():
+        print(str(k) + "\t: " + str(v))
+
+if __name__ == '__main__':
+    main()
